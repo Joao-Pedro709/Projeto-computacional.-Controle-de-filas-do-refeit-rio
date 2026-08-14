236549942c5dab539b275a67c37f6fda3bc0ef28
Saulo Oliveira <saulo.freitas.oliveira@gmail.com>
2026-08-14 10:26:03 -0300

    Add meal options to cardapio.py
    Foi o Luis Gustavo

---
+++ cardapio.py
@@ -0,0 +1,3 @@
+refeicoes = ["Arroz com frango, feijão e tomate\n", "Feijoada\n", "Macarronada com carne de sol\n", "Carne bovina com arroz, feijão e farofa\n", "Sopa de frango com torrada"]
+
+print(*refeicoes)
