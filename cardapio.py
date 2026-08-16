c08348ef7b27be3a4922df18bbd77037f1f30c00
Joao-Pedro709 <joaopedrolimafreitas321@gmail.com>
2026-08-16 13:48:07 -0300

    Delete cardapio.py

--- cardapio.py
+++
@@ -1,3 +0,0 @@
-refeicoes = ["Arroz com frango, feijão e tomate\n", "Feijoada\n", "Macarronada com carne de sol\n", "Carne bovina com arroz, feijão e farofa\n", "Sopa de frango com torrada", 'Strognoff de frango']
-
-print(*refeicoes)
