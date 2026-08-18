41c4c3cdd1fb3a212e9b75edd958a1df40de7177
Luis-Gustavo827 <118899075+Luis-Gustavo827@users.noreply.github.com>
2026-08-18 17:02:38 -0300

    Create Cardapio.py

---
+++ Cardapio.py
@@ -0,0 +1,3 @@
+refeicoes = ["Arroz com frango, feijÃ£o e tomate\n", "Feijoada\n", "Macarronada com carne de sol\n", "Carne bovina com arroz, feijÃ£o e farofa\n", "Sopa de frango com torrada"]
+
+print(*refeicoes)
