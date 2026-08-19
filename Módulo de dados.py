93fa570693b97bc97135ab58c8d928803067df55
Luis-Gustavo827 <118899075+Luis-Gustavo827@users.noreply.github.com>
2026-08-19 19:48:26 -0300

    Create Módulo de dados.py
    Feito por Luis Gustavo

---
+++ Módulo de dados.py
@@ -0,0 +1,11 @@
+usuarios = ["João", "Maria", "Pedro", "Ana", "Carlos"]
+
+horarios = ["6:00-7:00", "11:00-13:00", "15:00-16:00", "18:00-20:00"]
+
+refeicoes = [
+    "Arroz com frango, feijão e tomate",
+    "Feijoada",
+    "Macarronada com carne de sol",
+    "Carne bovina com arroz, feijão e farofa",
+    "Sopa de frango com torrada"
+]
