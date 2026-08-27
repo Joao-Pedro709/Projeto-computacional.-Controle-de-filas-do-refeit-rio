c23e32fa324ad27a1b3121ed078e57e2dbe838ce
Joao-Pedro709 <joaopedrolimafreitas321@gmail.com>
2026-08-27 17:00:39 -0300

    Atualizar o criar_funcao_de_fila.py

--- criar_funcao_de_fila.py
+++ criar_funcao_de_fila.py
@@ -1,24 +1,20 @@
-fila = []
+
+fila = [[],[]]#primeira lista pra quem é do grupo prioritario, e a segunda pra quem não é, já que na lista a prioridade é booleana
 
 
 def adicionar_usuario_e_ordenar(matricula,hora_entrada,status,prioridade):
-    
-    usuario = {
-        "matricula": matricula,
-        "hora_entrada": hora_entrada,
-        "status": status,
-        "prioridade": prioridade 
-    }
-    
-    fila.append(usuario)
-    fila.sort(key=lambda usuario: (not usuario["prioridade"], usuario["hora_entrada"]))
+    if prioridade == "1":
+        fila[0].append([matricula,hora_entrada,status,prioridade])
+        fila[0].sort(key=lambda x: x[1])#o sort usa o segundo elemento da sublista pra organizar em ordem de chegada
+    else:
+        fila[1].append([matricula,hora_entrada,status,prioridade])
+        fila[1].sort(key=lambda x: x[1])
     print(fila)
 
-
-
+   
 while True:
     adicionarM= input("digite a matrícula")
-    adicionarH= input("digite a hora de entrada")
+    adicionarH= int(input("digite a hora de entrada"))
     adicionarS= input("digite o status")
-    adicionarPr= int(input("digite a prioridade"))
+    adicionarPr= input("digite a prioridade")
     adicionar_usuario_e_ordenar(adicionarM,adicionarH,adicionarS, adicionarPr)
