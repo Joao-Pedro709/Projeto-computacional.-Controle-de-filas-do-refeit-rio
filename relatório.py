fb6b43baa5e66d3a2b7d98de950f1f0bdaaca464
P.vitor <118899097+PedroVitor21@users.noreply.github.com>
2026-09-11 04:09:33 +0000

    Add files via upload

---
+++ relatório.py
@@ -0,0 +1,12 @@
+manha= int(input("refeicoes manha"))
+tarde= int(nput("refeicoes tarde"))
+noite= int(input("refeicoes noite"))
+
+total_refeicoes= manha+tarde+noite
+print("quantidade refeicoes da manha",manha)
+
+print("quantidade refeicoes da tarfe",tarde)
+
+print("quantidade refeicoes da noite ", noite)
+
+print("quantidade total ",total_refeicoes)
