50be73383ee163ba11348fde4ecb59e2d2c2f96a
P.vitor <118899097+PedroVitor21@users.noreply.github.com>
2026-09-11 00:06:42 +0000

    Update criar_funcao_de_fila.py

--- criar_funcao_de_fila.py
+++ criar_funcao_de_fila.py
@@ -1,20 +1,18 @@
+import modulo_de_dados
 
-fila = [[],[]]#primeira lista pra quem é do grupo prioritario, e a segunda pra quem não é, já que na lista a prioridade é booleana
+fila = [[], []] #primeira lista pra quem é do grupo prioritario, e a segunda pra quem não é, já que na lista a prioridade é booleana
 
-
-def adicionar_usuario_e_ordenar(matricula,hora_entrada,status,prioridade):
+def adicionar_usuario_e_ordenar(matricula, hora_entrada, status, prioridade):
     if prioridade == "1":
-        fila[0].append([matricula,hora_entrada,status,prioridade])
-        fila[0].sort(key=lambda x: x[1])#o sort usa o segundo elemento da sublista pra organizar em ordem de chegada
+        fila[0].append([matricula, hora_entrada, status, prioridade])
+        fila[0].sort(key=lambda x: x[1])
     else:
-        fila[1].append([matricula,hora_entrada,status,prioridade])
+        fila[1].append([matricula, hora_entrada, status, prioridade])
         fila[1].sort(key=lambda x: x[1])
+    
+    arquivo_fila = open('fila_espera.txt', 'w')
+    arquivo_fila.write(str(fila))
+    arquivo_fila.close()
+    
     print(fila)
-
-   
-while True:
-    adicionarM= input("digite a matrícula")
-    adicionarH= int(input("digite a hora de entrada"))
-    adicionarS= input("digite o status")
-    adicionarPr= input("digite a prioridade")
-    adicionar_usuario_e_ordenar(adicionarM,adicionarH,adicionarS, adicionarPr)
+    
