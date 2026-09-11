0750fd142326b72081d75b77ed75d1e9589e320c
P.vitor <118899097+PedroVitor21@users.noreply.github.com>
2026-09-11 00:06:01 +0000

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,11 +1,21 @@
-lista = []
-while len(lista) < 10:
-    listaAdd = str(input('adicionar usuario: '))
-    if listaAdd != '':
-        lista.append(listaAdd)
-    else:
+def cadastrar():
+    usuarios = []
+    qntd_de_usuarios = 0
+
+    while qntd_de_usuarios <= 0 or qntd_de_usuarios > 10:
+        qntd_de_usuarios = int(input('Quantos usuários? (máximo 10): '))
+        if qntd_de_usuarios > 10 or qntd_de_usuarios <= 0:
+            print('Erro! Tente novamente.')
+
+    while len(usuarios) < qntd_de_usuarios:
+        listaAdd = input('Adicionar usuário: ').strip()
         while listaAdd == '':
-            listaAdd = str(input('tente novamente: '))
-print('lista de alunos')
-for contador in range(10):
-    print(f'{lista[contador]}, ', end='')
+            listaAdd = input('Nome inválido. Tente novamente: ').strip()
+        usuarios.append(listaAdd)
+
+    print('\nLista de alunos:')
+    for usuario in usuarios:
+        print(f'- {usuario}')
+
+    return usuarios
+    
