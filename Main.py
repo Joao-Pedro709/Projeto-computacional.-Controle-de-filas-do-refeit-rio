7335623b36d2c310a044e62cbe1a9bcd03ca7a46
P.vitor <118899097+PedroVitor21@users.noreply.github.com>
2026-09-11 00:05:02 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,35 +1,43 @@
-import Criar_funcao_de_fila
-import Situacoes_da_fila
-import Modulos_de_consulta
+from time import sleep
+import modulo_de_dados
+import criar_funcao_de_fila
 
-Situacoes_da_fila.carregar_dados()
+menu_decisao = ''
+while menu_decisao != '0':
+    sleep(1.0)
+    menu = '''
+    ╔═════════════════════════════════════════════════════════════════════╗
+    ║ SISTEMA DE CONTROLE DE FILA - RU v1.0                               ║
+    ╠═════════════════════════════════════════════════════════════════════╣
+    ║ [1] Cadastrar [1.5] ver cadastros                                   ║
+    ║ [2] Registrar Entrada na Fila (Com Checagem de Prioridade)          ║
+    ║ [3] Realizar Atendimento / Registrar Refeição                       ║
+    ║ [4] Relatório de Refeições Servidas por Turno                       ║
+    ║ [5] Salvar e Carregar Dados do Arquivo (refeitorio.pkl / csv)       ║
+    ║ [0] Sair do Sistema                                                 ║
+    ╚═════════════════════════════════════════════════════════════════════╝
+    '''
+    print(menu)
+    menu_decisao = input('digite a função: ').strip()
 
-us1 = Criar_funcao_de_fila.adicionar_usuario_e_ordenar(
-    "1",
-    "08:00",
-    "Aguardando",
-    1
-)
+    if menu_decisao == '1':
+        modulo_de_dados.modulo_de_dados()
 
-us2 = Criar_funcao_de_fila.adicionar_usuario_e_ordenar(
-    "2",
-    "08:10",
-    "Aguardando",
-    0
-)
+    elif menu_decisao == '1.5':
+        print(modulo_de_dados.lista_de_usuarios)
 
-fila = Criar_funcao_de_fila.fila
+    elif menu_decisao == '2':
+        a = input('Digite o nome do usuário: ')
+        if a in modulo_de_dados.lista_de_usuarios:
+            adicionarM = input("Digite a matrícula: ")
+            adicionaH = int(input("Digite a hora de entrada: "))
+            adicionarS = input("Digite o status: ")
+            adicionarPr = input("Digite a prioridade: ")
 
-posicao = Modulos_de_consulta.buscar_posicao(
-    fila,
-    "1"
-)
-
-print(f"Posição do usuário 1:  {posicao}   ")
-
-situacao = Modulos_de_consulta.consultar_situacao(
-    fila,
-    "1"
-)
-
-print(situacao["texto"])
+            criar_funcao_de_fila.adicionar_usuario_e_ordenar(
+                adicionarM, adicionaH, adicionarS, adicionarPr
+            )
+            print(criar_funcao_de_fila.fila)
+        else:
+            print("Usuário não encontrado nos cadastros.")
+            
